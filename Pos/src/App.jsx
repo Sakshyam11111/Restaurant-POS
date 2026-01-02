cc65655db3eb86adc110b02cf9ada2db6b10e074
Sakshyam11111 <sakshyamshrestha111@gmail.com>
2026-01-02 11:38:58 +0545

    Created Landing page

--- Pos/src/App.jsx
+++ Pos/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './component/mainpage/Landingpage';
+import JoinUs from './component/mainpage/JoinUs';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/joinus" element={<JoinUs />} />
         </Routes>
       </Router>
     </>
